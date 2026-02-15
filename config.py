666571e0873b1a55f4c028db7c7e044857de1499
crazynot010-cpu <crazynot010@gmail.com>
2026-02-15 20:22:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 CHAT_XP = 15
-ARISE_COOLDOWN = 2  # seconds
 SPAWN_MESSAGE_COUNT = 30
 SPAWN_CHANCE = 0.34
-DESPAWN_TIME = 300  # 5 minutes
+DESPAWN_TIME = 300
+ARISE_SUCCESS_RATE = 0.55
+MAX_TOTAL_SHADOWS = 16
+MAX_DUPLICATE_SHADOW = 3
