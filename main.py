2083b94684707af3d636b0ed520c94ae0d1c95bf
crazynot010-cpu <crazynot010@gmail.com>
2026-02-15 18:44:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import asyncio
+import asyncpg
+from config import TOKEN, DATABASE_URL
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+async def create_pool():
+    bot.pool = await asyncpg.create_pool(DATABASE_URL)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+
+async def load_cogs():
+    await bot.load_extension("cogs.profile")
+    await bot.load_extension("cogs.xp_system")
+    await bot.load_extension("cogs.shadow_spawn")
+    await bot.load_extension("cogs.admin")
+
+async def main():
+    async with bot:
+        await create_pool()
+        await load_cogs()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
